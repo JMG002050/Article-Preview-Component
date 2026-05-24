241aa67d0070725a1b7d40a95d208fe7b6b05a94
Jorge Martinez <jorge1austin1@gmail.com>
2026-05-24 13:44:08 -0500

    Completions

--- script.js
+++ script.js
@@ -1,34 +1,6 @@
-let spaceship = {
-   crew: {
-   captain: {
-       name: 'Lily',
-       degree: 'Computer Engineering',
-       cheerTeam() { console.log('You got this!') }
-       },
-   'chief officer': {
-       name: 'Dan',
-       degree: 'Aerospace Engineering',
-       agree() { console.log('I agree, captain!') }
-       },
-   medic: {
-       name: 'Clementine',
-       degree: 'Physics',
-       announce() { console.log(`Jets on!`) } },
-   translator: {
-       name: 'Shauna',
-       degree: 'Conservation Science',
-       powerFuel() { console.log('The tank is full!') }
-       }
-   }
-};
+const shareBtn = document.querySelector(".share-btn");
+const shareMenu = document.querySelector(".share-menu");
 
-
-// here crewRole is like the “let i” in a for loop. More exactly, it will be each key name in the object
-// for (let crewRole in spaceship.crew) {
-//  console.log(`${crewRole}: ${spaceship.crew[crewRole].name}`);
-// } 
-
- 
-// for (let crewDegree in spaceship.crew) {
-// console.log(`${spaceship.crew[crewDegree].name}: ${spaceship.crew[crewDegree].degree}`);
-// };
+shareBtn.addEventListener('click', () => {
+    shareMenu.classList.toggle("active");
+});
